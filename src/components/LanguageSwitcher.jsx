7c8470d8d866f398d429a7abfeb4a040a4db7d7a
ANDREKSL10 <andreksl@gmail.com>
2025-06-07 19:44:04 -0400

    Update LanguageSwitcher.jsx

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,1 +1,19 @@
+import { useRouter } from 'next/router';
+
+export default function LanguageSwitcher() {
+  const { locales, locale, asPath, push } = useRouter();
+
+  return (
+    <select
+      value={locale}
+      onChange={(e) => push(asPath, asPath, { locale: e.target.value })}
+    >
+      {locales.map((loc) => (
+        <option key={loc} value={loc}>
+          {loc.toUpperCase()}
+        </option>
+      ))}
+    </select>
+  );
+}
 
