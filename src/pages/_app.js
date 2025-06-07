1daa1bd3d34c294a4cc0c80ad16048e325d0be21
ANDREKSL10 <andreksl@gmail.com>
2025-06-07 19:43:41 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,1 +1,9 @@
+import { appWithTranslation } from 'next-i18next';
+import '../i18n';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default appWithTranslation(MyApp);
 
