e439b66a004c1c4548ea38c055a3ca7e16bb9973
ANDREKSL10 <andreksl@gmail.com>
2025-06-07 19:42:54 -0400

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,1 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './locales/en/common.json';
+import pt from './locales/pt/common.json';
+import es from './locales/es/common.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { common: en },
+      pt: { common: pt },
+      es: { common: es },
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    ns: ['common'],
+    defaultNS: 'common',
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
 
