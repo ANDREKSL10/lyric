c2f9a7f2c7727f52159c040701b6362413934048
ANDREKSL10 <andreksl@gmail.com>
2025-06-07 19:44:38 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,30 @@
+import { useTranslation } from 'react-i18next';
+import LanguageSwitcher from '../components/LanguageSwitcher';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+export default function Home() {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <header>
+        <LanguageSwitcher />
+        <h1>{t('header_title')}</h1>
+        <p>{t('slogan')}</p>
+      </header>
+      <main>
+        <input placeholder={t('input_placeholder')} />
+        <button>{t('search_button')}</button>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
+}
 
